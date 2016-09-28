622b1184228e3ebadb7eba9e05571ce11c45c27f
Donnie <donnie@tjuna.com>
2016-09-28 21:11:57 +0200

    The intro section of the site has a scroll down button that animates up and down. On click you will scroll down.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
 		$(this).toggleClass("open");
 		$("#menu-list").toggleClass("open");
 	});
+
+	$(".click-down").click(function() {
+		$('html, body').animate({
+		    scrollTop: $(".explain-product").offset().top
+		}, 1000);
+	});
 });
